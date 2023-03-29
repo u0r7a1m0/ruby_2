84086411bc690a87371baa1c935d36eed9c1d393
naka <114331695+u0r7a1m0@users.noreply.github.com>
2023-03-29 17:11:31 +0000

    お祭り途中

--- test.rb
+++ test.rb
@@ -1,3 +1,79 @@
+# y, m, d = gets.split.map(&:to_i)
+# a, b = gets.split.map(&:to_i)
+
+# y_num = y
+# while y_num % 4 != 1 do
+#     y_num += 1
+# end
+# year = y_num - y
+
+# #その他の月の合計日数
+# x = 0
+# if m > a
+#     month = (13 - m) + a
+#     year = year - 1
+    
+#     if m.even?
+#         total_month = (year * 13) + month
+#         puts total_month * 14 
+#         puts "①"
+#     else
+#         total_month = (year * 13) + month
+#         puts total_month * 14 
+#         puts "②"
+#     end
+    
+# else
+#     month = a.to_i - m.to_i
+#     if m.even?
+#         total_month = (year * 13) + month
+#         puts total_month * 14 
+#         puts "③"
+#     else
+#         total_month = (year * 13) + month
+#         puts total_month * 14 
+#         puts "④"
+        
+#     end    
+    
+# end
+
+
+# z = 0
+# if m.even? #現在偶数月？
+#     if a.even?
+#         z = z + (15 - d) - (15 - b)
+#     else
+#         z = z + (15 - d) - (13 - b)
+
+#     end
+# else
+#     if a.even?
+#         z = z + (13 - d) - (15 - b)
+#     else
+#         z = z + (13 - d) - (13 - b)
+#     end
+# end
+# # puts "#{year}年後、#{x + z}日後。"
+# # puts (year * 181) + (x + z)
+
+# 今が8月なら
+m = 8
+
+#全体の月数を year * 13 とする。
+
+
+day = 0
+
+
+    if m != 13 && m % 2 != 0
+        day += 15
+    else
+        day += 13
+    end
+
+
+
 y, m, d = gets.split.map(&:to_i)
 a, b = gets.split.map(&:to_i)
 
@@ -7,45 +83,69 @@ while y_num % 4 != 1 do
 end
 year = y_num - y
 
+#その他の月の合計日数
 x = 0
-if m > a
-    month = (13 - m) + a
-    year = year - 1
-    month.times do
-        if m == 13 || m.even?
-            x = x + 13
-        else # m.odd?
-            x = x + 15
-        end
-    end
-    x = x + 0
-else
-    month = a.to_i - m.to_i
-    month.times do
-        if m == 13 || m.even?
-            x = x + 13
-        else # m.odd?
-            x = x + 15
-        end
-    end
-    x = x + 2
-end
-
 z = 0
-if m.even? #現在偶数月？
-    if a.even?
-        z = z + (15 - d) - (15 - b)
+if m.to_i > a.to_i #年を超える
+    year = year - 1
+    years = year * 13
+    month = (13 - m) + a + years
+    puts month
+    month.times do
+        if m != 13 && m % 2 != 0
+            z += 15
+        else
+            z += 13
+        end
+    end
+    if m.even? #現在偶数月？
+        if a.even?
+            z = z + (15 - d) - (15 - b)
+        else
+            z = z + (15 - d) - (13 - b)
+    
+        end
     else
-        z = z + (15 - d) - (13 - b)
+        if a.even?
+            z = z + (13 - d) - (15 - b)
+        else
+            z = z + (13 - d) - (13 - b)
+        end
+    end
+    puts "(1)：OK"
 
-    end
 else
-    if a.even?
-        z = z + (13 - d) - (15 - b)
-    else
-        z = z + (13 - d) - (13 - b)
+    years = year * 13
+    month = a.to_i - m.to_i + years
+    puts month
+    month.times do
+        if m != 13 && m % 2 != 0
+            z += 15
+            # puts z
+        else
+            z += 13
+            # puts z
+        end
     end
+    if m.even? #現在偶数月？
+        if a.even?
+            z = z + (15 - d) - (15 - b)
+        else
+            z = z + (15 - d) - (13 - b)
+    
+        end
+    else
+        if a.even?
+            z = z + (13 - d) - (15 - b)
+        else
+            z = z + (13 - d) - (13 - b)
+        end
+    end
+    puts "(2)：まだ" 
 end
-# puts "#{year}年後、#{x + z}日後。"
-puts (year * 181) + (x + z)
+puts z
+puts "#{year}年後（#{years}ヶ月分)、#{month}ヶ月後！"
+
+
+
 
