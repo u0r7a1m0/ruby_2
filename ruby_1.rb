afd0fb82b147d169ce7523c85a3d847faf95b3e4
naka <114331695+u0r7a1m0@users.noreply.github.com>
2023-03-31 20:46:05 +0000

    リーグ表の作成完了

--- ruby_1.rb
+++ ruby_1.rb
@@ -1,27 +1,57 @@
-# # 自分の得意な言語で
-# # Let's チャレンジ！！
-# # N：参加者人数
-N = gets.to_i
-win = []
-lose = []
-
-all = []
-N.times do
-    vs = gets.split
-    win << vs[0]
-    lose << vs[1]
-    all << vs
+n = gets.to_i
+#回数
+m = n * (n - 1) / 2
+array = []
+m.times do
+    vs = gets.split.map(&:to_i)
+    array << vs
 end
 
-puts 
+# p array
+# 人数分の配列
+people = [*1..n]
+i = 0
+x = 0
+wl_array = []
+vs_array = []
 
-p "勝ち組"
-p win
+while n > i do
+    array.each do |a|
+        if a.include? people[i]
+            if a[0] == people[i]
+                vs_array << ["W", a[1]]
+            elsif
+                vs_array << ["L", a[0]]
+            end
+        else
+            unless vs_array.include? ["-", people[i]]
+            vs_array << ["-", people[i]]
+            end
+        end
+    end
+        # aとかbはvs_arrayでいう「"-", 1」「"L", 2」の一つ一つを表してる
+        # a[1]で「"-", 1」の「1」部分を表してる
+        wl_array << vs_array.sort{|a, b| a[1] <=> b[1]}
+        # wl_array << vs_array
+        # p vs_array[0]
 
-p "負け組"
-p lose
+        vs_array = []
+    i += 1
+end
 
-# p "全件"
-# p all[0..N-1]
+
+last_array = []
+
+wl_array.each do |w|
+    w.each do |wow|
+        # p wow[0]
+        last_array << wow[0]
+    end
+end
+o = 0
+n.times do
+    puts last_array.each_slice(n).to_a[o].join(" ")
+    o += 1
+end
 
 
